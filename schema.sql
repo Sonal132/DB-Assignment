3562bebcef597ca4b3388e41fe18912c5ef49d63
Sonal Gond <107026307+Sonal132@users.noreply.github.com>
2024-03-11 12:26:34 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,42 @@
+CREATE TABLE product_category (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP
+);
 
+CREATE TABLE product (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  SKU VARCHAR(255) NOT NULL,
+  category_id INT NOT NULL,
+  inventory_id INT,
+  price DECIMAL(10,2) NOT NULL,
+  discount_id INT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP,
+  FOREIGN KEY (category_id) REFERENCES product_category(id)
+);
+
+CREATE TABLE product_inventory (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  quantity INT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP
+);
+
+CREATE TABLE discount (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  discount_percent DECIMAL(5,2) NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP
+);
